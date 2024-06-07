c8848e0dac5c19af8764a208422af0ff287fd47d
Zoey <zoeykirkpatrick82@gmail.com>
2024-06-07 19:30:43 +1000

    changes

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from website import create_app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app = create_app()
