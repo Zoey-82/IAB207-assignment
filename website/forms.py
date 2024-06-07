7b3a95fc9f9f490d6bb722ef7bc789fea2839218
tarjy <jeeringxo@gmail.com>
2024-06-07 16:51:45 +1000

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -10,12 +10,15 @@ class LoginForm(FlaskForm):
 
  # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
+    user_name = StringField("User Name", validators=[InputRequired()])
     email = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    # linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
+    password = PasswordField("Password", validators=[InputRequired(), EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    contact_number = StringField('Contact Number', validators=[InputRequired()])
+    street_address = StringField('Street Address', validators=[InputRequired()])
+    submit = SubmitField('Register')
 
      # submit button
     submit = SubmitField("Register")
